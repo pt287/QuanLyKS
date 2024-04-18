1a67c36f3383c4c7a1decb27a728bb2ccd27ad11
capijim <153265171+capijim@users.noreply.github.com>
2024-04-18 16:01:47 +0700

    Add files via upload

---
+++ QLKS/src/DTO/NguoiDung/NhanVien.java
@@ -0,0 +1,50 @@
+package DTO.NguoiDung;
+
+
+
+public class NhanVien extends NguoiDung{
+    private String MaNhanVien;
+
+    public NhanVien(String MaNhanVien, String MaNguoiDung, String TenDangNhap, String MatKhau, String SDT, int TrangThai) {
+        super(MaNguoiDung, TenDangNhap, MatKhau, SDT, TrangThai);
+        this.MaNhanVien = MaNhanVien;
+    }
+    
+    public NhanVien(){}
+
+    public String getMaNhanVien() {
+        return MaNhanVien;
+    }
+
+    public void setMaNhanVien(String MaNhanVien) {
+        this.MaNhanVien = MaNhanVien;
+    }
+    
+    @Override
+    public void NhapThongTin() {
+        System.out.println("Nhap Thong Tin Nhan Vien:");;
+    }
+
+    @Override
+    public void LayThongTin() {
+        System.out.println("Lay Thong Tin Nhan Vien");;
+    }
+
+    /*@Override
+    public void SuaThongTin() {
+        System.out.println("");;
+    }*/
+    
+    @Override
+    public void DangNhap() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void DangXuat() {
+        throw new UnsupportedOperationException();
+    }
+
+    
+    
+}
