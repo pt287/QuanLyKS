54f995a24c235479acc993085a13862911275b04
TuiLaZit <147925219+TuiLaZit@users.noreply.github.com>
2024-04-22 14:33:29 +0700

    Add files via upload

---
+++ QLKS/src/DAO/NguoiDung/DAO/ConnectionProvider.java
@@ -0,0 +1,18 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionProvider {
+    public static Connection getCon(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useSSL=false","root","123456");
+            return con;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
