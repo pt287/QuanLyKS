61b9627909c19c7cfed3093888c6a88af7c86545
TuiLaZit <147925219+TuiLaZit@users.noreply.github.com>
2024-05-15 10:11:28 +0700

    Update HoaDonDAO.java

--- QLKS/src/DAO/HoaDonDAO.java
+++ QLKS/src/DAO/HoaDonDAO.java
@@ -16,8 +16,7 @@ public class HoaDonDAO {
         try {
             st=con.createStatement();
             st.executeUpdate("Set Foreign_key_checks = 0");
-            String qry="Insert into HoaDon Values(";
-            qry = qry + hd.getMaHoaDon() +",";
+            String qry="Insert into HoaDon (WkID,ClientID,DateIn,DateOut,BillTotal) Values(";
             qry = qry +"'" + hd.getMaNhanVien()+"',";
             qry = qry +"'" + hd.getMaKhachHang()+"',";
             qry = qry +"'" + hd.getNgayNhan() + "',";
@@ -52,5 +51,49 @@ public class HoaDonDAO {
             //JOptionPane.ShowMessageDialog(null,"Lỗi đọc thông tin Sinh Viên!");
         }
         return dshd;
+    }
+    public int[] ClientPerMonth(){
+        int [] arr = new int[12];
+        try{
+            st=con.createStatement();
+            rs=st.executeQuery("select month(DateIn)as month ,count(month(DateIn)) as SoBill from hoadon group by month(DateIn)");
+            while(rs.next()){
+                arr[rs.getInt(1)-1] = rs.getInt(2);
+            }
+        }
+        catch(SQLException ex){
+            //báo lỗi
+        }
+        return arr;
+    }
+    
+    public int[] MoneyPerMonth(){
+        int [] arr = new int [12];
+        try{
+            st= con.createStatement();
+            rs=st.executeQuery("select month(DateIN) as month, sum(BillTotal) as TongTien from hoadon group by month(DateIn)");
+            while(rs.next()){
+                arr[rs.getInt(1)-1] = rs.getInt(2);
+            }
+        }
+        catch(SQLException ex){
+            //báo lỗi
+        }
+        return arr;
+    }
+    public int[][] CPSAR(){
+        int[][] arr = new int [12][2];
+        try{
+            st = con.createStatement();
+            rs = st.executeQuery("select month(DateIn) as month, count(SVCID) as SV, count(RoomID) as Room from ChiTietHoaDon, HoaDon Where HoaDon.BillID=chitiethoadon.BillID Group by month(DateIn);");
+            while(rs.next()){
+                arr[rs.getInt(1)-1][0]=rs.getInt(2);
+                arr[rs.getInt(1)-1][1]=rs.getInt(3);
+            }
+        }
+        catch(SQLException ex){
+            
+        }
+        return arr;
     }
 }
