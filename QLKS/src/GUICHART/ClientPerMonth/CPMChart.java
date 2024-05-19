c09073401119476eb8d0894b22c28605ba115495
TuiLaZit <147925219+TuiLaZit@users.noreply.github.com>
2024-05-19 22:15:34 +0700

    Add files via upload

--- QLKS/src/GUICHART/ClientPerMonth/CPMChart.java
+++ QLKS/src/GUICHART/ClientPerMonth/CPMChart.java
@@ -72,14 +72,17 @@ public class CPMChart extends javax.swing.JPanel {
         CPMchart = new GUICHART.raven.curvechart.CurveLineChart();
         jScrollPane1 = new javax.swing.JScrollPane();
         CPMTable = new javax.swing.JTable();
+        Refresh = new javax.swing.JButton();
 
         setBackground(new java.awt.Color(220, 242, 197));
+        setMaximumSize(null);
+        setMinimumSize(null);
         setPreferredSize(new java.awt.Dimension(1720, 1020));
 
         CPMchart.setForeground(new java.awt.Color(0, 16, 31));
 
         CPMTable.setBackground(new java.awt.Color(220, 242, 197));
-        CPMTable.setFont(new java.awt.Font("Segoe UI", 0, 26)); // NOI18N
+        CPMTable.setFont(new java.awt.Font("Segoe UI", 0, 20)); // NOI18N
         CPMTable.setForeground(new java.awt.Color(0, 16, 31));
         CPMTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -100,35 +103,62 @@ public class CPMChart extends javax.swing.JPanel {
                 return types [columnIndex];
             }
         });
-        CPMTable.setRowHeight(76);
+        CPMTable.setRowHeight(57);
         jScrollPane1.setViewportView(CPMTable);
+        CPMTable.getAccessibleContext().setAccessibleName("800");
+        CPMTable.getAccessibleContext().setAccessibleDescription("700");
+
+        Refresh.setBackground(new java.awt.Color(0, 16, 31));
+        Refresh.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        Refresh.setForeground(new java.awt.Color(220, 242, 197));
+        Refresh.setText("Làm Mới");
+        Refresh.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                RefreshActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(CPMchart, javax.swing.GroupLayout.PREFERRED_SIZE, 864, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 838, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(16, 16, 16)
+                        .addComponent(CPMchart, javax.swing.GroupLayout.PREFERRED_SIZE, 700, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 500, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(Refresh, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(CPMchart, javax.swing.GroupLayout.PREFERRED_SIZE, 937, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 937, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(9, 9, 9)
+                .addComponent(Refresh, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 700, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(CPMchart, javax.swing.GroupLayout.PREFERRED_SIZE, 700, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(28, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void RefreshActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RefreshActionPerformed
+        // TODO add your handling code here:
+        CPMchart.clear();
+        DataTable();
+        DataChart();
+    }//GEN-LAST:event_RefreshActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTable CPMTable;
     private GUICHART.raven.curvechart.CurveLineChart CPMchart;
+    private javax.swing.JButton Refresh;
     private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 }
